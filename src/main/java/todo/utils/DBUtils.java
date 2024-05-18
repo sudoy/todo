dfe4e5d80b88c6f97870c77658449d5ea1c5685d
sudoy <yuichi.sudo@ssie.jp>
2024-05-18 13:14:37 +0900

    本番DB設定

--- src/main/java/todo/utils/DBUtils.java
+++ src/main/java/todo/utils/DBUtils.java
@@ -1,22 +1,41 @@
 package todo.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 public class DBUtils {
+	private static final String DSN = "jdbc:mysql://us-cluster-east-01.k8s.cleardb.net/heroku_5d2133367028194?reconnect=true&characterEncoding=UTF-8&characterSetResults=UTF-8";
+	private static final String USER = "b3a786857413f1";
+	private static final String PASSWORD = "9fb86b1d";
+	
 	public static Connection getConnection() throws NamingException, SQLException {
 		//データベースの接続を確立
-	    Context initContext = new InitialContext();
-	    Context envContext  = (Context)initContext.lookup("java:/comp/env");
-	    DataSource ds = (DataSource)envContext.lookup("jdbc/mysql");
-	    return ds.getConnection();
+//	    Context initContext = new InitialContext();
+//	    Context envContext  = (Context)initContext.lookup("java:/comp/env");
+//	    DataSource ds = (DataSource)envContext.lookup("jdbc/mysql");
+//	    return ds.getConnection();
+	    
+	    Connection conn = null;
+		
+		try {
+			// JDBCドライバのロード
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// データベースへ接続
+			conn = DriverManager.getConnection(DSN, USER, PASSWORD);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
 	}
 	
 	public static void close(Connection con, PreparedStatement ps, ResultSet rs) {
